6e9292ee36ea710a6241a8e1b6d5fc2a9b749729
udeshika123-cyber <udeshikaprabhashini21@gmail.com>
2025-05-09 21:31:35 +0530

    This is s chatbot program

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { WhatsappService } from './whatsapp.service';
 
 @Controller('whatsapp')
 export class WhatsappController {
-    public constructor(private readonly whatsappService:WhatsappService){}
+  public constructor(private readonly whatsappService: WhatsappService) {}
 
-    @Post('send')
-    sendMessage(@Req()request:Request){
-        console.log(JSON.stringify(request.body));
-        return this.whatsappService.sendMessage();
+  @Get('send')
+  sendMessage(@Req() request: Request) {
+    console.log(JSON.stringify(request.body));
+    return this.whatsappService.sendMessage();
+  }
+
+  @Get('webhook')
+  challengeWebhook(@Req() req: Request, @Res() res: Response) {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    // Check if mode and token exist
+    if (mode && token) {
+      if (mode === 'subscribe' && token === '123456789') {
+        console.log('WEBHOOK_VERIFIED');
+        return res.status(200).send(challenge);
+      } else {
+        return res.sendStatus(403);
+      }
     }
+
+    return res.sendStatus(400);
+  }
+  @Post('webhook')
+  handleWebhook(@Req() req: Request, @Res() res: Response){
+    console.log('Received webhook:',JSON.stringify(req.body.entry));
+    res.sendStatus(200);
+  }
 }
--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class WhatsappService {
     sendMessage(){
-        return 'HELLO World';
+        return 'HELLO WORLD';
     }
 }
